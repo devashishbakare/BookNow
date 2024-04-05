aca13d202697a73574937ed108dce1f4d549aea9
devashish bakare <devashishbakare@gmail.com>
2024-04-05 11:50:32 +0530

    added swagger Swagger annotation for login API's

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,127 @@ const router = express.Router();
 const homeController = require("../controller/homeController");
 const loginController = require("../controller/loginController");
 router.get("/", homeController.testAPI);
+
+/**
+ * @swagger
+ * /signUp:
+ *   post:
+ *     summary: Register User with sign up form
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               confirm_password:
+ *                 type: string
+ *               phone_number:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User registration successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description : user register successfull message
+ *                 data:
+ *                   type: string
+ *                   description : jwt token
+ *                 userDetails :
+ *                  type : object
+ *                  description : user document for checking perpose
+ *                 userSchema:
+ *                   $ref: './model/userSchema.js'
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
+
 router.post("/signUp", loginController.signUp);
+
+/**
+ * @swagger
+ * /signIn:
+ *   post:
+ *     summary: login user
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *
+ *     responses:
+ *       200:
+ *         description: User registration successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description : user register successfull message
+ *                 data:
+ *                   type: string
+ *                   description : jwt token
+ *                 userDetails :
+ *                  type : object
+ *                  description : user document for checking perpose
+ *                 userSchema:
+ *                   $ref: './model/userSchema.js'
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
+
 router.post("/signIn", loginController.signIn);
 module.exports = router;
