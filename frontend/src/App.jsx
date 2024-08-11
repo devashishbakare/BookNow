f851209a57939531f724b73ec961e5d4a937358e
devashish bakare <devashishbakare@gmail.com>
2024-08-11 11:33:09 +0530

    frontend build faild patch-1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { City } from "./Components/City";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Hotel } from "./Components/Hotel";
 import { SignInHomePage } from "./Components/SignInHomePage";
-import { UpdateImages } from "./Components/updateImages";
+// import { UpdateImages } from "./Components/updateImages";
 import { BookingConfirmation } from "./Components/BookingConfirmation";
 // import { ProfilePage } from "./Components/ProfilePage";
 import { ConfirmBooking } from "./Components/ConfirmBooking";
@@ -20,7 +20,7 @@ function App() {
           <Route path="/city/:cityName" element={<City />} />
           <Route path="/hotel/:hotelId" element={<Hotel />} />
           <Route path="/signIn" element={<SignInHomePage />} />
-          <Route path="/uploadImages" element={<UpdateImages />} />
+          {/* <Route path="/uploadImages" element={<UpdateImages />} /> */}
           {/* <Route path="/profile" element={<ProfilePage />} /> */}
           <Route path="/profile" element={<Profile />} />
           <Route path="/bookHotel/:hotelId" element={<ConfirmBooking />} />
